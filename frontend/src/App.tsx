ded31e06958cfa95febc0ab23a5f8cd0954e7530
bartfastiel <24777648+bartfastiel@users.noreply.github.com>
2024-04-29 22:48:35 +0200

    Fix movement

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import TileCard from "./TileCard.tsx";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import axios from "axios";
 import {Tile} from "./Tile.ts";
 
@@ -14,9 +14,13 @@ const movements = {
 function App() {
 
     const [tiles, setTiles] = useState<Tile[]>([])
-    const [scoutX, setScoutX] = useState(0)
-    const [scoutY, setScoutY] = useState(0)
-    const [movement, setMovement] = useState<{start: number, direction: { x: -1 | 0 | 1, y: -1 | 0 | 1 }}>()
+    const [movement, setMovement] = useState<{
+        start: number,
+        startPosition: DOMRect,
+        direction: { x: -1 | 0 | 1, y: -1 | 0 | 1 }
+    }>()
+
+    const scoutRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         axios.get('/api/tiles')
@@ -26,43 +30,75 @@ function App() {
     }, [])
 
     const handleKeyDown = (event: KeyboardEvent) => {
+        const scout = scoutRef.current;
+        if (!scout) {
+            return;
+        }
+
         const direction = movements[event.key as keyof typeof movements];
         if (direction) {
             setMovement({
                 start: Date.now(),
+                startPosition: scout.getBoundingClientRect(),
                 direction,
             })
             event.preventDefault()
         }
     };
 
-    // whenever a frame is requested (requestAnimationFrame), calculate the position of the scout
+    const handleKeyUp = (event: KeyboardEvent) => {
+        const direction = movements[event.key as keyof typeof movements];
+        if (direction) {
+            setMovement(undefined);
+            event.preventDefault()
+        }
+    };
+
+    function calculatePosition() {
+        if (!movement) {
+            return;
+        }
+        const now = Date.now();
+        const timePassed = now - movement.start;
+        const speed = 0.1;
+        const distance = speed * timePassed;
+        return {
+            x: movement.startPosition.x + distance * movement.direction.x,
+            y: movement.startPosition.y + distance * movement.direction.y,
+        };
+    }
+
+    const moveScout = () => {
+        const scout = scoutRef.current;
+        if (!scout) {
+            return;
+        }
+        const distance = calculatePosition();
+        if (!distance) {
+            return;
+        }
+
+        scout.style.left = `${distance.x}px`;
+        scout.style.top = `${distance.y}px`;
+
+        requestAnimationFrame(() => moveScout());
+    };
+
     useEffect(() => {
         if (!movement) {
             return;
         }
 
-        const now = Date.now();
-        const timePassed = now - movement.start;
-        const speed = 0.1;
-        const distance = speed * timePassed;
-
-        setScoutX((prev) => prev + movement.direction.x * distance);
-        setScoutY((prev) => prev + movement.direction.y * distance);
-
-        requestAnimationFrame(() => {
-            setMovement({
-                start: now,
-                direction: movement.direction,
-            });
-        });
+        requestAnimationFrame(() => moveScout());
     }, [movement]);
 
     useEffect(() => {
         window.addEventListener('keydown', handleKeyDown);
+        window.addEventListener('keyup', handleKeyUp);
 
         return () => {
             window.removeEventListener('keydown', handleKeyDown);
+            window.removeEventListener('keyup', handleKeyUp);
         };
     }, []);
 
@@ -73,7 +109,11 @@ function App() {
                     return <TileCard tile={tile}/>
                 })
             }
-            <TileCard tile={{x: scoutX, y: scoutY, type: "scout"}}/>
+            <div className="scout" ref={scoutRef} style={{
+                position: "absolute",
+            }}>
+                <TileCard tile={{x: 0, y: 0, type: "scout"}}/>
+            </div>
         </>
     )
 }
