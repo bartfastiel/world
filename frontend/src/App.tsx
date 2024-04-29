8d042f978bc5c5229e7629281f006eaa60e95715
bartfastiel <24777648+bartfastiel@users.noreply.github.com>
2024-04-29 20:49:42 +0200

    Add second grass tile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,16 @@ import './App.css'
 function App() {
 
     return (
-        <img src="ME_Singles_City_Props_16x16_Shrub_2.png"></img>
+        <>
+            <img src="ME_Singles_City_Props_16x16_Shrub_2.png" className="tile" style={{
+                top: '0px',
+                left: '0px',
+            }}></img>
+            <img src="ME_Singles_City_Props_16x16_Shrub_2.png" className="tile" style={{
+                top: '0px',
+                left: '16px',
+            }}></img>
+        </>
     )
 }
 
