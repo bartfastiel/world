ae2db4812fe3b764904c42f83f6818cc6d100f29
bartfastiel <24777648+bartfastiel@users.noreply.github.com>
2024-04-29 20:51:48 +0200

    Show 10x10 tiles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,14 +4,16 @@ function App() {
 
     return (
         <>
-            <img src="ME_Singles_City_Props_16x16_Shrub_2.png" className="tile" style={{
-                top: '0px',
-                left: '0px',
-            }}></img>
-            <img src="ME_Singles_City_Props_16x16_Shrub_2.png" className="tile" style={{
-                top: '0px',
-                left: '16px',
-            }}></img>
+            {
+                Array.from({ length: 10 }, (_, i) => i).map((row) => {
+                    return Array.from({ length: 10 }, (_, i) => i).map((col) => {
+                        return <img src="ME_Singles_City_Props_16x16_Shrub_2.png" className="tile" style={{
+                            top: `${row * 16}px`,
+                            left: `${col * 16}px`,
+                        }}></img>
+                    })
+                })
+            }
         </>
     )
 }
